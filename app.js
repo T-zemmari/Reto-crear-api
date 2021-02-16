5fe83919d47172c199dbb7929af15ff810e308cc
T-zemmari <76817211+T-zemmari@users.noreply.github.com>
2021-02-16 11:34:19 +0100

    hemos teseado la class Store

--- app.js
+++ app.js
@@ -5,3 +5,5 @@ const app = express();
 const port = 3000;
 
 app.listen(3000,console.log(`estoy escuchando en el puero ${port}`));
+
+
---
+++ prueba_test_de_store/prueba.js
@@ -0,0 +1,26 @@
+const Store = require('./Store');
+
+ let store = new Store();
+
+ 
+
+ store.set({name:'tarek',age:40});
+ store.set({name:'tarek',age:40});
+ store.set({name:'tarek',age:40});
+ store.set({name:'tarek',age:40});
+ store.set({name:'tarek',age:40});
+ store.set({name:'tarek',age:40});
+ store.set({name:'tarek',age:40});
+ store.set({name:'tarek',age:40});
+ store.set({name:'tarek',age:40});
+ store.set({name:'tarek',age:40});
+ store.set({name:'tarek',age:40});
+ store.set({name:'lola',age:25});
+ store.set({name:'blanca',age:18});
+
+ store.set({name:'ana',age:33});
+
+
+ resultado3 =store.get();
+
+ console.log(resultado3);
