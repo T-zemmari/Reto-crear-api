8cde824cd977c437353f566d7099069d19b08903
T-zemmari <76817211+T-zemmari@users.noreply.github.com>
2021-02-17 13:20:17 +0100

    nos hemos encontrado con una piedra, hay que solucionarla

--- models/car.js
+++ models/car.js
@@ -16,4 +16,21 @@ class Car{
 
 }
 
+
+
+const coche1 = new Car();
+      coche1.brand="mercedes";
+      coche1.model="class C220";
+      coche1.year= 2021;
+
+
+ const coche2 = new Car();
+      coche2.brand= "KIA";
+      coche2.model= "sorento";
+      coche2.year= 2020;     
+
+console.log(coche1,coche2);
+
+
+
 module.exports = Car;
